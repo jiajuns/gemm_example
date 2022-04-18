681c1736223648e014ecde676065f60e9ce02285
ubuntu <ubuntu@localhost.localdomain>
2022-04-18 15:51:46 +0800

    cublas benchmark

--- src/main.cu
+++ src/main.cu
@@ -2,87 +2,105 @@
 #include <cublas_v2.h>
 #include <iostream>
 #include <stdio.h>
+#include <sys/time.h>
 
 using namespace std;
 
+constexpr int b_ = 1024;
+constexpr int m_ = 1024;
+constexpr int n_ = 1024;
+constexpr int k_ = 1024;
+constexpr int n_iter = 50;
 
-__global__
-void show(float* ptr, int size) {
-        for(int i =0; i<size; i++)
-        printf("%f\n", ptr[i]);
-}
+int cublas_test(float* a, float* b, float* c) {
+    float* d_a, *d_b, *d_c;
+    size_t size_a = sizeof(float) * b_ * m_ * k_;
+    size_t size_b = sizeof(float) * b_ * n_ * k_;
+    size_t size_c = sizeof(float) * b_ * m_ * n_;
+    cudaMalloc(&d_a, size_a);
+    cudaMalloc(&d_b, size_b);
+    cudaMalloc(&d_c, size_c);
+
+    cudaMemcpy(d_a, a, size_a, cudaMemcpyHostToDevice);
+    cudaMemcpy(d_b, b, size_b, cudaMemcpyHostToDevice);
+    cudaMemcpy(d_c, c, size_c, cudaMemcpyHostToDevice);
+
+    cublasHandle_t handle;
+    cublasStatus_t ret;
+    ret = cublasCreate(&handle);
+
+    float *a_array[b_], *b_array[b_];
+    float *c_array[b_];
+    for (int i = 0; i < b_; ++i) {
+        a_array[i] = d_a + i * m_ * k_;
+        b_array[i] = d_b + i * k_ * n_;
+        c_array[i] = d_c + i * m_ * n_;
+    }
+    const float **d_Aarray, **d_Barray;
+    float **d_Carray;
+    cudaMalloc((void**)&d_Aarray, b_*sizeof(float *));
+    cudaMalloc((void**)&d_Barray, b_*sizeof(float *));
+    cudaMalloc((void**)&d_Carray, b_*sizeof(float *));
+    cudaMemcpy(d_Aarray, a_array, b_*sizeof(float *), cudaMemcpyHostToDevice);
+    cudaMemcpy(d_Barray, b_array, b_*sizeof(float *), cudaMemcpyHostToDevice);
+    cudaMemcpy(d_Carray, c_array, b_*sizeof(float *), cudaMemcpyHostToDevice);
+
+    const float alpha  =  1.0f;
+    const float beta  =  0.0f;
+    int m = m_;
+    int n = n_;
+    int k = k_;
+    int lda = m_;
+    int ldb = k_;
+    int ldc = m_;
+    int batch = b_;
+
+    cout << "start cublasSgemmBatched benchmark" << endl;
+
+    struct timeval start, end;
+    gettimeofday(&start, NULL);
+    for (int i = 0; i < n_iter; i++) {
+        ret = cublasSgemmBatched(handle,
+                            CUBLAS_OP_N,
+                            CUBLAS_OP_N,
+                            m,n,k,
+                            &alpha,
+                            d_Aarray,  lda,
+                            d_Barray,  ldb,
+                            &beta,
+                            d_Carray,  ldc,
+                            batch);
+    }
+    gettimeofday(&end, NULL);
+
+    // if (ret == CUBLAS_STATUS_SUCCESS) {
+    //     printf("sgemm success  %d, line(%d)\n", ret, __LINE__);
+    // }
+    printf("[INFO] batch_size %d m %d n %d k %d cublasSgemmBatched-time %.5f ms ( %d iterations) \n", b_, m_, n_, k_,
+          ((end.tv_sec - start.tv_sec) * 1000 + (end.tv_usec - start.tv_usec) * 0.001) / n_iter, n_iter);
+
+    cublasDestroy(handle);
+    cudaFree(d_a);
+    cudaFree(d_b);
+    cudaFree(d_c);
+    cudaFree(d_Aarray);
+    cudaFree(d_Barray);
+    cudaFree(d_Carray);
+    return 0;
+};
 
 int main() {
 
-        float* a = new float[16];
-        for(int i=0; i<16; i++) a[i] = 1.0;
+    float* a = new float[b_ * m_ * k_];
+    for(int i = 0; i < b_ * m_ * k_; i++) a[i] = i;
 
-        float* b = new float[32];
-        for(int i=0; i<32; i++) b[i] = i+1;
+    float* b = new float[b_ * k_ * n_];
+    for(int i = 0; i < b_ * k_ * n_; i++) b[i] = i+1;
 
-        float* c = new float[16];
-        for(int i=0; i<16; i++) c[i] = 3.0;
+    float* c = new float[b_ * m_ * n_];
+    for(int i = 0; i < b_ * m_ * n_; i++) c[i] = 0.0;
 
-        float* d_a, *d_b, *d_c;
-        size_t size = sizeof(float) * 16;
-        cudaMalloc(&d_a, size);
-        cudaMalloc(&d_b, size*2);
-        cudaMalloc(&d_c, size);
+    cublas_test(a, b, c);
 
-        cudaMemcpy(d_a, a, size, cudaMemcpyHostToDevice);
-        cudaMemcpy(d_b, b, size*2, cudaMemcpyHostToDevice);
-        cudaMemcpy(d_c, c, size, cudaMemcpyHostToDevice);
-
-
-        cublasHandle_t handle;
-        cublasStatus_t ret;
-        ret = cublasCreate(&handle);
-        float *a_array[8], *b_array[8];
-        float *c_array[8];
-        for (int i = 0; i < 2; ++i) {
-            for (int j = 0; j < 4; ++j) {
-                a_array[i*4+j] = d_a + i * 8 + j * 2;
-                b_array[i*4+j] = d_b + i * 2 * 8  + j * 2;
-                c_array[i*4+j] = d_c + i * 8 + j * 2;
-            }
-        }
-        const float **d_Marray, **d_Narray;
-        float **d_Parray;
-        cudaMalloc((void**)&d_Marray, 8*sizeof(float *));
-        cudaMalloc((void**)&d_Narray, 16*sizeof(float *));
-        cudaMalloc((void**)&d_Parray, 8*sizeof(float *));
-        cudaMemcpy(d_Marray, a_array, 8*sizeof(float *), cudaMemcpyHostToDevice);
-        cudaMemcpy(d_Narray, b_array, 16*sizeof(float *), cudaMemcpyHostToDevice);
-        cudaMemcpy(d_Parray, c_array, 8*sizeof(float *), cudaMemcpyHostToDevice);
-
-
-        const float alpha  =  1.0f;
-        const float beta  =  0.0f;
-        int m = 2;
-        int n = 1;
-        int k = 2;
-        int lda = 8;
-        int ldb = 8;
-        int ldc = 8;
-        int batch = 8;
-
-       ret = cublasSgemmBatched(handle,
-                           CUBLAS_OP_N,
-                           CUBLAS_OP_N,
-                           m,n,k,
-                           &alpha,
-                           d_Narray,  ldb,
-                           d_Marray,  lda,
-                           &beta,
-                           d_Parray,  ldc,
-                           batch);
-        cublasDestroy(handle);
-        if (ret == CUBLAS_STATUS_SUCCESS) {
-            printf("sgemm success  %d, line(%d)\n", ret, __LINE__);
-        }
-
-        show<<<1,1>>>(c_array[0], 16);
-        cudaMemcpy(c, d_c, size, cudaMemcpyDeviceToHost);
-        for(int i=0; i<16; i++) cout<<c[i]<<" "<<endl;
-        return 0;
+    return 0;
 }
